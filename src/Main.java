ce0be4949920751decf93d0cbe7d7ee310cc5ef1
Yael <yael.rodriguez@iteso.mx>
2023-01-26 14:08:48 -0600

    En eso andamos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
+
+
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
     }
+
+
+    static void practica_1(){
+
+    }
+
+
+    static void practica_2(){
+
+    }
+
+
+    static void practica_3(){
+
+    }
+
 }
